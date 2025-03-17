6e68c9e9bfaa96b5db671acbd43ad32ea34ebb62
allen <boseallen192@gmail.com>
2025-03-17 12:26:24 +0530

    1 created login page

--- helloWorld/urls.py
+++ helloWorld/urls.py
@@ -17,11 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+
+import userAuthentication
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.homePage),
     path('about/',views.aboutPage),
-    path('students/',include('Students.urls'))
+    path('students/',include('Students.urls')),
+    path('login/',include('userAuthentication.urls'))
 ]
--- userAuthentication/views.py
+++ userAuthentication/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login
+from django.contrib import messages
+from userAuthentication.forms import LoginForm
+
 
 # Create your views here.
+def getLoginPage(request):
+    if(request.method=='GET'):
+
+        form=LoginForm
+        context={
+            "form":form
+        }
+
+        return render(request,"login.html",context)
+
+    elif(request.method=='POST'):
+        loginForm=LoginForm(request.POST)
+
+        if(loginForm.is_valid()):
+            name=loginForm.cleaned_data["userName"]
+            password=loginForm.cleaned_data["password"]
+
+            user=authenticate(request,username=name,password=password)
+
+            if user:
+                login(request,user)
+                return redirect("/students")
+
+            messages.error(request,"invalid username or password!")
+            context={
+                "form":loginForm
+            }
+
+            return render(request,"login.html",context)
+
+        
+
+
